3cb312f6440b1a6f96701a26d8582a20b0b420a5
rahat <rahatbuddy@gmail.com>
2020-09-15 12:28:08 +0600

    Initial Commit

---
+++ gitPrac/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitPrac
+//
+//  Created by Md istiak on 15/9/20.
+//  Copyright © 2020 Md istiak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ gitPracTests/gitPracTests.swift
@@ -0,0 +1,34 @@
+//
+//  gitPracTests.swift
+//  gitPracTests
+//
+//  Created by Md istiak on 15/9/20.
+//  Copyright © 2020 Md istiak. All rights reserved.
+//
+
+import XCTest
+@testable import gitPrac
+
+class gitPracTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
